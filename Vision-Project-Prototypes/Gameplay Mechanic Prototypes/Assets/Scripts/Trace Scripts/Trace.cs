7bab26db77b1813cb7a45a3eb7e0bcf1b9fe9031
louiej3 <gotacoolemail@yahoo.com>
2015-04-21 16:29:11 -0700

    Start Trace scene

---
+++ Vision-Project-Prototypes/Gameplay Mechanic Prototypes/Assets/Scripts/Trace Scripts/Trace.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trace : MonoBehaviour 
+{
+
+	GameObject trail;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		trail = new GameObject();
+		trail.transform.position = Vector2.zero;
+		trail.AddComponent<TrailRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.touchCount > 0)
+		{
+			Touch touch = Input.GetTouch(0);
+			trail.transform.position = Vector2.MoveTowards(transform.position, touchPos, maxFollowSpeed);
+		}
+	}
+}
