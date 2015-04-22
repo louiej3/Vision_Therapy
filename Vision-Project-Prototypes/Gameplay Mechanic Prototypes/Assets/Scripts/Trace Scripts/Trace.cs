2579614d26e888127e3ac46e498af79ceb86c2f0
louiej3 <gotacoolemail@yahoo.com>
2015-04-22 14:21:09 -0700

    Trace script instantiates its own TrailRenderer object

--- Vision-Project-Prototypes/Gameplay Mechanic Prototypes/Assets/Scripts/Trace Scripts/Trace.cs
+++ Vision-Project-Prototypes/Gameplay Mechanic Prototypes/Assets/Scripts/Trace Scripts/Trace.cs
@@ -4,7 +4,8 @@ using System.Collections;
 public class Trace : MonoBehaviour 
 {
 
-	GameObject trail;
+	private GameObject trail;
+	private float maxFollowSpeed = 400f;
 	
 	// Use this for initialization
 	void Start () 
@@ -20,7 +21,16 @@ public class Trace : MonoBehaviour
 		if (Input.touchCount > 0)
 		{
 			Touch touch = Input.GetTouch(0);
+			Vector3 worldPoint = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+			Vector2 touchPos = new Vector2(worldPoint.x, worldPoint.y);
+
+			trail.GetComponent<TrailRenderer>().time = 5f;
 			trail.transform.position = Vector2.MoveTowards(transform.position, touchPos, maxFollowSpeed);
+
+			if (touch.phase == TouchPhase.Ended)
+			{
+				trail.GetComponent<TrailRenderer>().time = 0f;
+			}
 		}
 	}
 }
