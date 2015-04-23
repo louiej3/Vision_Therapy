7b8ae1bc7bb69abe15acc36c199cd358d84ee9b0
louiej3 <gotacoolemail@yahoo.com>
2015-04-23 15:32:08 -0700

    Add function to create single Trace Route segment between two points

---
+++ Vision-Project-Prototypes/Gameplay Mechanic Prototypes/Assets/Scripts/Trace Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour 
+{
+
+	TraceRoute tr;
+
+	// Use this for initialization
+	void Start () 
+	{
+		tr = GameObject.Find("GameManager").GetComponent<TraceRoute>();
+		
+		Vector2 p1 = new Vector2(1, 0);
+		Vector2 p2 = new Vector2(4, 4);
+		tr.createSegment(p1, p2);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
---
+++ Vision-Project-Prototypes/Gameplay Mechanic Prototypes/Assets/Scripts/Trace Scripts/TraceRoute.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TraceRoute : MonoBehaviour 
+{
+
+	protected GameObject segment;
+	protected GameObject p1;
+	protected GameObject p2;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	public void createSegment(Vector2 point1, Vector2 point2)
+	{
+		p1 = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+		p1.transform.position = point1;
+		p2 = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+		p2.transform.position = point2;
+		
+		segment = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		Vector2 newPoint = (point1 + point2) / 2;
+		segment.transform.position = newPoint;
+		float stretch = Vector2.Distance(point1, point2);
+		segment.transform.localScale = new Vector3(1f, 1f, stretch);
+		segment.transform.LookAt(point2);
+	}
+}
