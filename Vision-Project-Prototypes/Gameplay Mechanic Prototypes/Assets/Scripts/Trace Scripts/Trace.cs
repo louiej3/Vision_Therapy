d14d527cb6d126ae40105d15b4fe554aa271da71
louiej3 <gotacoolemail@yahoo.com>
2015-04-22 22:52:52 -0700

    Fix bug where trace line jumps from one finger to another

--- Vision-Project-Prototypes/Gameplay Mechanic Prototypes/Assets/Scripts/Trace Scripts/Trace.cs
+++ Vision-Project-Prototypes/Gameplay Mechanic Prototypes/Assets/Scripts/Trace Scripts/Trace.cs
@@ -6,6 +6,9 @@ public class Trace : MonoBehaviour
 
 	private GameObject trail;
 	private float maxFollowSpeed = 400f;
+	private int previousFingerId = -1;
+	private int currentFingerId = -1;
+	private bool firstTouch = false;
 	
 	// Use this for initialization
 	void Start () 
@@ -13,6 +16,7 @@ public class Trace : MonoBehaviour
 		trail = new GameObject();
 		trail.transform.position = Vector2.zero;
 		trail.AddComponent<TrailRenderer>();
+		trail.GetComponent<TrailRenderer>().time = 0f;
 	}
 	
 	// Update is called once per frame
@@ -24,13 +28,32 @@ public class Trace : MonoBehaviour
 			Vector3 worldPoint = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
 			Vector2 touchPos = new Vector2(worldPoint.x, worldPoint.y);
 
-			trail.GetComponent<TrailRenderer>().time = 5f;
-			trail.transform.position = Vector2.MoveTowards(transform.position, touchPos, maxFollowSpeed);
+			if (Input.touchCount == 1)
+			{
+				currentFingerId = touch.fingerId;
+			}
 
-			if (touch.phase == TouchPhase.Ended)
+			if (touch.phase == TouchPhase.Began && currentFingerId == touch.fingerId)
 			{
 				trail.GetComponent<TrailRenderer>().time = 0f;
+				trail.transform.position = touchPos;
+				trail.GetComponent<TrailRenderer>().time = 5f;
+				Debug.Log("Began touch");
 			}
+			else if (touch.phase == TouchPhase.Ended)
+			{
+				trail.GetComponent<TrailRenderer>().time = 0f;
+				Debug.Log("Ended touch");
+			}
+			else if (currentFingerId == touch.fingerId)
+			{
+				trail.transform.position = Vector2.MoveTowards(transform.position, touchPos, maxFollowSpeed);
+				Debug.Log("Moved touch");
+			}
+		}
+		else
+		{
+			currentFingerId = -1;
 		}
 	}
 }
