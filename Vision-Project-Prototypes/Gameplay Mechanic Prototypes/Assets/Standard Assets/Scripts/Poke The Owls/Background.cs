10a31ee7bd407fae75e2d4465af2c610393831d1
louiej3 <gotacoolemail@yahoo.com>
2015-05-03 21:38:46 -0700

    Add owl and background sprite

---
+++ Vision-Project-Prototypes/Gameplay Mechanic Prototypes/Assets/Standard Assets/Scripts/Poke The Owls/Background.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Background : MonoBehaviour 
+{
+
+	public float speed = -50f;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.Rotate(Vector3.forward, speed * Time.deltaTime);
+	}
+}
