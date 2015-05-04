4a1e4e91276d5e3086dcd0dddeaa70ea6db06ede
louiej3 <gotacoolemail@yahoo.com>
2015-05-04 15:21:48 -0700

    Add comments

--- Vision-Project-Prototypes/Gameplay Mechanic Prototypes/Assets/Standard Assets/Scripts/Poke The Owls/GameManager.cs
+++ Vision-Project-Prototypes/Gameplay Mechanic Prototypes/Assets/Standard Assets/Scripts/Poke The Owls/GameManager.cs
@@ -33,21 +33,28 @@ public class GameManager : MonoBehaviour
 
 	public void spawnOwl()
 	{
+		// Instantiate the owl prefab
 		Target owl = Instantiate(Resources.Load("Poke The Owls/Owl", typeof(Target))) as Target;
 		
+		// Generate random x position
 		float worldHeight = Camera.main.orthographicSize - owl.transform.lossyScale.y / 2;
 		float x = Random.Range(-worldHeight, worldHeight);
 		
+		// Generate random y position
 		float worldWidth = Mathf.Sqrt(Mathf.Pow(worldHeight, 2) - Mathf.Pow(x, 2));
 		float y = Random.Range(-worldWidth, worldWidth);
 
+		// Generate random speed
 		float speed = Random.Range(minSpeed, maxSpeed);
 
+		// Position and set owl speed
 		owl.transform.position = new Vector2(x, y);
 		owl.GetComponent<OrbitMove>().SPEEDFACTOR = speed;
 
+		// Add owl to target manager
 		targetMan.addTarget(owl);
 		
+		// Restart the spawn timer
 		timer.start();
 	}
 }
