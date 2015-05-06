8950008bbd08c6f538f50dc4b74e7bb58e83a668
louiej3 <gotacoolemail@yahoo.com>
2015-05-05 17:44:32 -0700

    Add converging ring scene and script

--- Vision-Project-Prototypes/Gameplay Mechanic Prototypes/Assets/Standard Assets/Scripts/Converging Rings/ConvergingObjects.cs
+++ Vision-Project-Prototypes/Gameplay Mechanic Prototypes/Assets/Standard Assets/Scripts/Converging Rings/ConvergingObjects.cs
@@ -4,11 +4,14 @@ using System.Collections;
 public class ConvergingObjects : MonoBehaviour 
 {
 
-	GameObject point;
-	GameObject ring1;
-	GameObject ring2;
-	Vector2 newVec1;
-	Vector2 newVec2;
+	private GameObject point;
+	public GameObject ring1;
+	public GameObject ring2;
+	public float time = 5f;
+	private float startTime = 0f;
+	public float ring1Speed = 0f;
+	public float ring2Speed = 0f;
+	private TextMesh timeDisplay;
 	
 	// Use this for initialization
 	void Start () 
@@ -22,17 +25,29 @@ public class ConvergingObjects : MonoBehaviour
 		ring2 = Instantiate(Resources.Load("Converging Rings Prefabs/Ring", typeof(GameObject))) as GameObject;
 		ring2.transform.position = new Vector2(-3f, 1f);
 
-		newVec1 = Vector2.zero;
-		newVec2 = Vector2.zero;
+		float ring1Distance = Vector2.Distance(ring1.transform.position, point.transform.position);
+		float ring2Distance = Vector2.Distance(ring2.transform.position, point.transform.position);
+
+		ring1Speed = ring1Distance / time;
+		ring2Speed = ring2Distance / time;
+
+		Vector2 ring1Direction = (Vector2)point.transform.position - (Vector2)ring1.transform.position;
+		ring1.transform.LookAt(ring1.transform.position + new Vector3(0, 0, 1), ring1Direction);
+
+		Vector2 ring2Direction = (Vector2)point.transform.position - (Vector2)ring2.transform.position;
+		ring2.transform.LookAt(ring2.transform.position + new Vector3(0, 0, 1), ring2Direction);
+
+		timeDisplay = GameObject.Find("TimeDisplay").GetComponent<TextMesh>();
+
+		startTime = Time.realtimeSinceStartup;
 	}
 	
 	// Update is called once per frame
-	void FixedUpdate () 
-	{	
-		ring1.transform.position = Vector2.SmoothDamp(ring1.transform.position,
-			point.transform.position, ref newVec1, 5f);
+	void Update () 
+	{
+		ring1.transform.position += ring1.transform.up * ring1Speed * Time.smoothDeltaTime;
+		ring2.transform.position += ring2.transform.up * ring2Speed * Time.smoothDeltaTime;
 
-		ring2.transform.position = Vector2.SmoothDamp(ring2.transform.position,
-			point.transform.position, ref newVec2, 5f);
+		timeDisplay.text = "Converge in = " + (Time.realtimeSinceStartup - startTime);
 	}
 }
