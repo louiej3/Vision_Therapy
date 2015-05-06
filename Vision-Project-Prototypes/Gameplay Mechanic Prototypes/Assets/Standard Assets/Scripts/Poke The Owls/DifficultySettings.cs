563dff5d10fb487012e262e7b56bdab63e9d1e5e
louiej3 <gotacoolemail@yahoo.com>
2015-05-05 17:46:17 -0700

    Add DifficultySettings class

---
+++ Vision-Project-Prototypes/Gameplay Mechanic Prototypes/Assets/Standard Assets/Scripts/Poke The Owls/DifficultySettings.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DifficultySettings : MonoBehaviour 
+{
+
+	public int maxOwlsOnScreen = 5;
+	public float minOwlSpeed = 0.5f;
+	public float maxOwlSpeed = 1.5f;
+	public float owlSize = 1f;
+	public float owlOpacity = 1f;
+	public float spiralOpacity = 1f;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
