b1504bc8e5d256068fe24e5d8eb85541c6bf2bed
louiej3 <gotacoolemail@yahoo.com>
2015-05-05 17:55:06 -0700

    Comment and organize DifficultSettings

--- Vision-Project-Prototypes/Gameplay Mechanic Prototypes/Assets/Standard Assets/Scripts/Poke The Owls/DifficultySettings.cs
+++ Vision-Project-Prototypes/Gameplay Mechanic Prototypes/Assets/Standard Assets/Scripts/Poke The Owls/DifficultySettings.cs
@@ -4,15 +4,27 @@ using System.Collections;
 public class DifficultySettings : MonoBehaviour 
 {
 
+	// The maximum number of owls that can be on the
+	// screen at once.
 	public int maxOwlsOnScreen = 5;
+	// The scale of the owls. The owls are squares.
+	public float owlScale = 1f;
+	// The transperancy of the owls
+	public float owlOpacity = 1f;
+	// The range that the owls' speed can be
 	public float minOwlSpeed = 0.5f;
 	public float maxOwlSpeed = 1.5f;
-	public float owlSize = 1f;
-	public float owlOpacity = 1f;
-	public float spiralOpacity = 1f;
+	// The time before an owl disappears
 	public float owlTimeout = 5f;
+	// The time between each new owl being spawned
 	public float owlSpawnInterval = 3f;
+
+	// The transperancy of the spiral
+	public float spiralOpacity = 1f;
+	// The speed that the spiral spins
 	public float spiralSpeed = -80f;
+
+	// The number of owls needed to win
 	public int owlsToWin = 20;
 
 }
