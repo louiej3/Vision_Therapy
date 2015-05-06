f9d2ac471e3f994d1fbc3f8d2604628bea2a43ab
louiej3 <gotacoolemail@yahoo.com>
2015-05-05 18:56:37 -0700

    Add opacity to Background and Owl

--- Vision-Project-Prototypes/Gameplay Mechanic Prototypes/Assets/Standard Assets/Scripts/Poke The Owls/Background.cs
+++ Vision-Project-Prototypes/Gameplay Mechanic Prototypes/Assets/Standard Assets/Scripts/Poke The Owls/Background.cs
@@ -4,12 +4,16 @@ using System.Collections;
 public class Background : MonoBehaviour 
 {
 
-	public float speed;
+	private float speed;
+	private float opacity;
 
 	// Use this for initialization
 	void Start () 
 	{
 		speed = GameObject.Find("GameManager").GetComponent<DifficultySettings>().spiralSpeed;
+		
+		opacity = GameObject.Find("GameManager").GetComponent<DifficultySettings>().spiralOpacity;
+		GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, opacity);
 	}
 	
 	// Update is called once per frame
--- Vision-Project-Prototypes/Gameplay Mechanic Prototypes/Assets/Standard Assets/Scripts/Poke The Owls/Owl.cs
+++ Vision-Project-Prototypes/Gameplay Mechanic Prototypes/Assets/Standard Assets/Scripts/Poke The Owls/Owl.cs
@@ -6,13 +6,19 @@ public class Owl : Target
 
 	private float timeout;
 	private float scale;
+	private float opacity;
 
 	protected override void Start()
 	{
 		base.Start();
+		
 		timeout = GameObject.Find("GameManager").GetComponent<DifficultySettings>().owlTimeout;
+		
 		scale = GameObject.Find("GameManager").GetComponent<DifficultySettings>().owlScale;
 		transform.localScale = new Vector2(scale, scale);
+
+		opacity = GameObject.Find("GameManager").GetComponent<DifficultySettings>().owlOpacity;
+		GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, opacity);
 	}
 
 	// Update is called once per frame
