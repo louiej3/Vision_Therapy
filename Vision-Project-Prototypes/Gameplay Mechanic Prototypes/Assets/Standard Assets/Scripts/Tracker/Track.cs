06d99d7b0c85af1c6d73648ac3ca12a41f390fa2
John Louie <gotacoolemail@yahoo.com>
2015-05-27 00:36:29 -0700

    Add Dummy and Track scripts

---
+++ Vision-Project-Prototypes/Gameplay Mechanic Prototypes/Assets/Standard Assets/Scripts/Tracker/Track.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Track : Target 
+{
+	protected override void tapBehavior()
+	{
+		// Turn this object green on touch
+		GetComponent<SpriteRenderer>().color = new Color(0f, 255f, 0f, 
+			GetComponent<SpriteRenderer>().color.a);
+	}
+}
